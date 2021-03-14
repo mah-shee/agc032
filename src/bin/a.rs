6e26a6f5a32833394d3f186b296e4a0cb2b9c53c
masa <masatoshi.sboard@gmail.com>
2021-03-14 19:06:00 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,28 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+    n: usize,
+    mut b:[usize;n],
+    }
+    let mut ans = Vec::<usize>::new();
+    let mut key = true;
+    for i in 0..n {
+        for j in (0..b.len()).rev() {
+            if j + 1 == b[j] {
+                ans.push(b[j]);
+                b.remove(j);
+                break;
+            }
+        }
+        if n - i == b.len() {
+            key = false;
+        }
+    }
+    if key {
+        for i in ans.iter().rev() {
+            println!("{}", i);
+        }
+    } else {
+        println!("-1");
     }
 }
